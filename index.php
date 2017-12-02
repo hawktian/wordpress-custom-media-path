4ccdd99ef84d5e3ef508eecfd622e4d03b63f813
Tonvin Tian <itonvin@gmail.com>
2017-12-02 10:19:00 +0800

    Init

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: WordPress custom media upload
+Description: Custom Image saved path and name.
+Author: Tonvin Tian<itonvin@gmail.com>
+*/
+class Custom_Media_Save {
+    public function __construct() {
+        add_filter( 'upload_dir', array($this, 'upload_dir'), 1, 1);
+        add_filter( 'sanitize_file_name', array($this, 'sanitize_file_name'), 100, 1);
+    }
+    public function upload_dir($config) {
+        $day = date('d', time());
+        return array(
+                'path'   => $config['path'] . '/'.$day,
+                'url'    => $config['url'] . '/'.$day,
+                'subdir' => $config['subdir']. '/'.$day,
+                ) + $config;
+    }
+    public function sanitize_file_name( $filename ) {
+        $info = pathinfo( $filename );
+        return substr(md5(time().uniqid()), 5, 10).'.'.$info['extension'];
+    }
+}
+new Custom_Media_Save();
