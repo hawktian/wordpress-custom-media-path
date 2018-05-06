5db5304ba0bae9c39814c035cb8ce6d33a90cec3
Tonvin Tian <itonvin@gmail.com>
2018-05-07 06:46:10 +0800

    custom upload url path

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ class Custom_Media_Save {
     public function __construct() {
         add_filter( 'upload_dir', array($this, 'upload_dir'), 1, 1);
         add_filter( 'sanitize_file_name', array($this, 'sanitize_file_name'), 100, 1);
+        add_filter( 'pre_option_upload_url_path', array($this, 'custom_upload_url_path'));
     }
     public function upload_dir($config) {
         $day = date('d', time());
@@ -19,9 +20,17 @@ class Custom_Media_Save {
                 'subdir' => $config['subdir']. '/'.$day,
                 ) + $config;
     }
+
     public function sanitize_file_name( $filename ) {
         $info = pathinfo( $filename );
         return substr(md5(time().uniqid()), 5, 10).'.'.$info['extension'];
+    }
+
+    public function custom_upload_url_path( $upload_url_path ) {
+        if ( defined('UPLOAD_URL_PATH') ) {
+            return UPLOAD_URL_PATH;
+        }
+        return false;
     }
 }
 new Custom_Media_Save();
