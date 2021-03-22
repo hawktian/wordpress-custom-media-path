a229fc7c34b018854840628efebd6da36d4d7f53
Tonvin Tian <itonvin@gmail.com>
2021-03-22 08:44:13 +0000

    update comment

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
 /*
- * Plugin Name: Custom Meida Save Directory
- * Plugin URI: https://github.com/Tonvin/wordpress-custom-media-upload
- * Description: Custom uploaded medias saved directory and name.
+ * Plugin Name: Custom Media Path
+ * Plugin URI: https://github.com/Tonvin/wordpress-custom-media-path
+ * Description: Change medias stored directory and name.
  * Author: Tonvin Tian<itonvin@gmail.com>
  * Author URI: https://tonvin.net/
  **/
 
-class Custom_Media_Upload {
+class Custom_Media_Path {
     public function __construct() {
         add_filter( 'upload_dir', array($this, 'upload_dir'), 1, 1);
         add_filter( 'sanitize_file_name', array($this, 'sanitize_file_name'), 100, 1);
     }
+
+    //append directory named by day
     public function upload_dir($config) {
         $day = date('d', time());
         return array(
@@ -21,11 +23,12 @@ class Custom_Media_Upload {
                 ) + $config;
     }
 
+    //change file name to random string
     public function sanitize_file_name( $filename ) {
         $info = pathinfo( $filename );
         return substr(md5(time().uniqid()), 5, 10).'.'.$info['extension'];
     }
 
 }
-new Custom_Media_Upload();
+new Custom_Media_Path();
 
